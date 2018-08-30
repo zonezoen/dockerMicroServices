642c591dff84dde3279a17d355b32579f6442605
zone <842221695@qq.com>
2018-08-30 20:04:00 +0800

    add thrift file

--- moudel/moudelUser/api/server.py
+++ moudel/moudelUser/api/server.py
@@ -8,10 +8,12 @@ from thrift.server import TServer
 
 
 class UserHandler:
+    # user = UserService.User
+
     def __init__(self):
         pass
 
-    def registUser(self, user):
+    def registUser(self, username, password, sex, age):
         print("registUser")
 
     def login(self, username, password):
@@ -21,16 +23,17 @@ class UserHandler:
         print("logout")
 
 
-# 创建服务端
-handler = UserHandler()
-processor = UserService.Processor(handler)
-# 监听端口
-transport = TSocket.TServerSocket("127.0.0.1", 3000)
-# 选择传输层
-tfactory = TTransport.TBufferedTransportFactory()
-# 选择传输协议
-pfactory = TBinaryProtocol.TBinaryProtocolFactory()
-# 创建服务端
-server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
-print("Starting thrift server in python...")
-server.serve()
+if __name__ == '__main__':
+    # 创建服务端
+    handler = UserHandler()
+    processor = UserService.Processor(handler)
+    # 监听端口
+    transport = TSocket.TServerSocket("127.0.0.1", 3000)
+    # 选择传输层
+    tfactory = TTransport.TBufferedTransportFactory()
+    # 选择传输协议
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+    # 创建服务端
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    print("Starting thrift server in python...")
+    server.serve()
